165dea7b6e0ce3b6f3165c82c1f1e78da5b348b3
Chris Gutierrez <cmgson1@gmail.com>
2021-04-28 11:46:19 -0500

    set up header, main, and wrapper

---
+++ employee-directory/src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DataArea from "./DataArea";
+
+class Main extends Component {
+    render() {
+        return (
+            <>
+                <DataArea />
+            </>
+        );
+    }
+}
+
+export default Main;
